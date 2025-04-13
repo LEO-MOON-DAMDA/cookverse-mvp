8f993c2345636e873b00c21a1b01b33a0e603d74
LEO-MOON-DAMDA <bcmoon79@gmail.com>
2025-04-13 12:13:57 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,20 @@
+// app/layout.tsx
+
+export const metadata = {
+  title: "Cookverse",
+  description: "A recipe sharing platform for the world",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
 
